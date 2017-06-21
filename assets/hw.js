3377fe9b707cd09ce0474e94da9ff5bb4a9c02a0
Andy Bonner <abonner@duke.edu>
2017-06-21 14:10:14 -0400

    pseudocode

---
+++ assets/hw.js
@@ -0,0 +1,10 @@
+/*
+Pseudocode:
+1. establish a target div for the buttons and one for the images
+2. establish the form
+3. hard-code the starting array
+4. write the function to add an individual button from the array to the display
+5. call this function for every element in the array
+6. write the function to take the form input, add it to the array, and add it to the page
+7. write the function to start/stop on click?
+*/
